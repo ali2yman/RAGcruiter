9a0241131dcd442a357033ff6f04372f504243f8
ali2yman <ali.ayman5755@gmail.com>
2025-06-13 02:31:42 +0300

    adding Update and process and MongoSetup

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,13 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
 
 class Settings(BaseSettings):
+    APP_NAME: str
+    APP_VERSION: str
+    # OPENAI_API_KEY: str
 
-    APP_NAME : str
-    APP_VERSION : str
+    FILE_ALLOWED_TYPES: List[str]  
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
 
-    class Config:
-        env_file = ".env"
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
+
+    model_config = SettingsConfigDict(env_file=".env")  #  Pydantic v2 style
 
 def get_settings():
     return Settings()
-
